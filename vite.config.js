13736b608596209624d5de28b3380fb301acecf3
Fathur Rahman <cyberflyx@gmail.com>
2026-01-15 20:29:09 +0800

    feat: configure multiple entry points for a multi-page build.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig(({ command }) => ({
   base: "/learnvite/",
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pricing: resolve(__dirname, "pricing.html"),
+        about: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
+  },
 }));
