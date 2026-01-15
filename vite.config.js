48e82d5af8f8c9a5b48c8b6c7a65b102b8409bac
Fathur Rahman <cyberflyx@gmail.com>
2026-01-15 20:16:27 +0800

    fix relative link

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/learnvite/" : "/",
+  base: "/learnvite/",
   plugins: [tailwindcss()],
 }));
